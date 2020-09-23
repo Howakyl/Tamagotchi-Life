6757fb3a914b7b6b3cc1e0d4187edd6d58ab5a32
Howakyl96 <howakyl@gmail.com>
2020-09-23 11:20:49 -0700

    Commit 12 - created Pet class

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+
+
+
+
+// Parent class for Tamagotchis, with params of hunger, boredom, and sleep)
+class Pet {
+    constructor (hunger, boredom, sleep) {
+        this.hunger = hunger;
+        this.boredom = boredom;
+        this.sleep = sleep;
+    }
+};
+
